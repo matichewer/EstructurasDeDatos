ab9877e1b79394d281ea29e30b285db9b07b5186
matichewer <schwerdt.matias@gmail.com>
2021-08-20 10:37:33 -0300

    Correccion de comentarios en TDA Diccionario

--- src/TDADiccionario/Dictionary.java
+++ src/TDADiccionario/Dictionary.java
@@ -4,14 +4,14 @@ package TDADiccionario;
 public interface Dictionary<K,V>
 {
 	/**
-	 * Consulta el n�mero de entradas del diccionario.
-	 * @return N�mero de entradas del diccionario.
+	 * Consulta el numero de entradas del diccionario.
+	 * @return Numero de entradas del diccionario.
 	 */
 	public int size();
 	
 	/**
-	 * Consulta si el diccionario est� vac�o.
-	 * @return Verdadero si el diccionario est� vac�o, falso en caso contrario.
+	 * Consulta si el diccionario está vacio.
+	 * @return Verdadero si el diccionario está vacio, falso en caso contrario.
 	 */
 	public boolean isEmpty();
 	
@@ -19,15 +19,15 @@ public interface Dictionary<K,V>
 	 * Busca una entrada con clave igual a una clave dada y la devuelve, si no existe retorna nulo.
 	 * @param key Clave a buscar.
 	 * @return Entrada encontrada.
-	 * @throws InvalidKeyException si la clave pasada por par�metro es inv�lida.
+	 * @throws InvalidKeyException si la clave pasada por parametro es invalida.
 	 */
 	public Entry<K,V> find(K key) throws InvalidKeyException;
 	
 	/**
-	 * Retorna una colecci�n iterable que contiene todas las entradas con clave igual a una clave dada.
+	 * Retorna una coleccion iterable que contiene todas las entradas con clave igual a una clave dada.
 	 * @param key Clave de las entradas a buscar.
-	 * @return Colecci�n iterable de las entradas encontradas.
-	 * @throws InvalidKeyException si la clave pasada por par�metro es inv�lida.
+	 * @return Coleccion iterable de las entradas encontradas.
+	 * @throws InvalidKeyException si la clave pasada por parametro es invalida.
 	 */
 	public Iterable<Entry<K,V>> findAll(K key) throws InvalidKeyException;
 	
@@ -35,7 +35,7 @@ public interface Dictionary<K,V>
 	 * Inserta una entrada con una clave y un valor dado en el diccionario y retorna la entrada creada.
 	 * @param key Clave de la entrada a crear.
 	 * @return value Valor de la entrada a crear.
-	 * @throws InvalidKeyException si la clave pasada por par�metro es inv�lida.
+	 * @throws InvalidKeyException si la clave pasada por parametro es invalida.
 	 */
 	public Entry<K,V> insert(K key, V value) throws InvalidKeyException;
 	
@@ -43,13 +43,13 @@ public interface Dictionary<K,V>
 	 * Remueve una entrada dada en el diccionario y devuelve la entrada removida.
 	 * @param e Entrada a remover.
 	 * @return Entrada removida.
-	 * @throws InvalidEntryException si la entrada no est� en el diccionario o es inv�lida.
+	 * @throws InvalidEntryException si la entrada no está en el diccionario o es invalida.
 	 */
 	public Entry<K,V> remove(Entry<K,V> e) throws InvalidEntryException;
 	
 	/**
-	 * Retorna una colecci�n iterable con todas las entradas en el diccionario.
-	 * @return Colecci�n iterable de todas las entradas.
+	 * Retorna una coleccion iterable con todas las entradas en el diccionario.
+	 * @return Coleccion iterable de todas las entradas.
 	 */
 	public Iterable<Entry<K,V>> entries();
 	
