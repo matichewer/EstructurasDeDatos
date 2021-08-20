93579a7e37b88d1bd8399052044e8d69040a9b1d
matichewer <schwerdt.matias@gmail.com>
2021-08-20 10:32:12 -0300

    Correccion de comentarios en TDA Grafo

--- src/TDAGrafo/Graph.java
+++ src/TDAGrafo/Graph.java
@@ -6,92 +6,92 @@ package TDAGrafo;
 public interface Graph<V,E> {
 	
 	/**
-	 * Devuelve una colecci�n iterable de v�rtices.
-	 * @return Una colecci�n iterable de v�rtices.
+	 * Devuelve una coleccion iterable de vertices.
+	 * @return Una coleccion iterable de vertices.
 	 */
 	public Iterable<Vertex<V>> vertices();
 	
 	/**
-	 * Devuelve una colecci�n iterable de arcos.
-	 * @return Una colecci�n iterable de arcos.
+	 * Devuelve una coleccion iterable de arcos.
+	 * @return Una coleccion iterable de arcos.
 	 */
 	public Iterable<Edge<E>> edges();
 	
 	/**
-	 * Devuelve una colecci�n iterable de arcos incidentes a un v�rtice v.
-	 * @param v Un v�rtice.
-	 * @return Una colecci�n iterable de arcos incidentes a un v�rtice v.
-	 * @throws InvalidVertexException si el v�rtice es inv�lido.
+	 * Devuelve una coleccion iterable de arcos incidentes a un vertice v.
+	 * @param v Un vertice.
+	 * @return Una coleccion iterable de arcos incidentes a un vertice v.
+	 * @throws InvalidVertexException si el vertice es invalido.
 	 */
 	public Iterable<Edge<E>> incidentEdges(Vertex<V> v) throws InvalidVertexException;
 	
 	
 	/**
-	 * Devuelve el v�rtice opuesto a un Arco E y un v�rtice V.
-	 * @param v Un v�rtice
+	 * Devuelve el vertice opuesto a un Arco E y un vertice V.
+	 * @param v Un vertice
 	 * @param e Un arco
-	 * @return El v�rtice opuesto a un Arco E y un v�rtice V.
-	 * @throws InvalidVertexException si el v�rtice es inv�lido.
-	 * @throws InvalidEdgeException si el arco es inv�lido.
+	 * @return El vertice opuesto a un Arco E y un vertice V.
+	 * @throws InvalidVertexException si el vertice es invalido.
+	 * @throws InvalidEdgeException si el arco es invalido.
 	 */
 	public Vertex<V> opposite(Vertex<V> v, Edge<E> e) throws InvalidVertexException, InvalidEdgeException;
 	
 	/**
-	 * Devuelve un Arreglo de 2 elementos con lo v�rtices extremos de un Arco e.
+	 * Devuelve un Arreglo de 2 elementos con lo vertices extremos de un Arco e.
 	 * @param  e Un arco
 	 * @return Un Arreglo de 2 elementos con los extremos de un Arco e.
-	 * @throws InvalidEdgeException si el arco es inv�lido.
+	 * @throws InvalidEdgeException si el arco es invalido.
 	 */
 	public Vertex<V> [] endvertices(Edge<E> e) throws InvalidEdgeException;
 	
 	/**
-	 * Devuelve verdadero si el v�rtice w es adyacente al v�rtice v.
-	 * @param v Un v�rtice
-	 * @param w Un v�rtice
-	 * @return Verdadero si el v�rtice w es adyacente al v�rtice v, falso en caso contrario.
-	 * @throws InvalidVertexException si uno de los v�rtices es inv�lido.
+	 * Devuelve verdadero si el vertice w es adyacente al vertice v.
+	 * @param v Un vertice
+	 * @param w Un vertice
+	 * @return Verdadero si el vertice w es adyacente al vertice v, falso en caso contrario.
+	 * @throws InvalidVertexException si uno de los vertices es invalido.
 	 */
 	public boolean areAdjacent(Vertex<V> v,Vertex<V> w) throws InvalidVertexException;
 	
 	/**
-	 * Reemplaza el r�tulo de v por un r�tulo x.
-	 * @param v Un v�rtice
-	 * @param x R�tulo
-	 * @return El r�tulo anterior del v�rtice v al reemplazarlo por un r�tulo x.
-	 * @throws InvalidVertexException si el v�rtice es inv�lido.
+	 * Reemplaza el rotulo de v por un rotulo x.
+	 * @param v Un vertice
+	 * @param x Rotulo
+	 * @return El rotulo anterior del vertice v al reemplazarlo por un rotulo x.
+	 * @throws InvalidVertexException si el vertice es invalido.
 	 */
 	public V replace(Vertex<V> v, V x) throws InvalidVertexException;
 	
 	/**
-	 * Inserta un nuevo v�rtice con r�tulo x.
-	 * @param x r�tulo del nuevo v�rtice
-	 * @return Un nuevo v�rtice insertado.
+	 * Inserta un nuevo vertice con rotulo x.
+	 * @param x rotulo del nuevo vertice
+	 * @return Un nuevo vertice insertado.
 	 */
 	public Vertex<V> insertVertex(V x);
 	
 	/**
-	 * Inserta un nuevo arco con r�tulo e, con v�rtices extremos v y w.
-	 * @param v Un v�rtice
-	 * @param w Un v�rtice
-	 * @param e r�tulo del nuevo arco.
+	 * Inserta un nuevo arco con rotulo e, con vertices extremos v y w.
+	 * @param v Un vertice
+	 * @param w Un vertice
+	 * @param e rotulo del nuevo arco.
 	 * @return Un nuevo arco.
-	 * @throws InvalidVertexException si uno de los v�rtices es inv�lido.
+	 * @throws InvalidVertexException si uno de los vertices es invalido.
 	 */
 	public Edge<E> insertEdge(Vertex<V> v, Vertex<V> w, E e) throws InvalidVertexException;
 	
 	/**
-	 * Remueve un v�rtice V y retorna su r�tulo.
-	 * @param v Un v�rtice
-	 * @return r�tulo de V.
-	 * @throws InvalidVertexException si el v�rtice es inv�lido.
+	 * Remueve un vertice V y retorna su rotulo.
+	 * @param v Un vertice
+	 * @return rotulo de V.
+	 * @throws InvalidVertexException si el vertice es invalido.
 	 */
 	public V removeVertex(Vertex<V> v) throws InvalidVertexException;
 	
 	/**
-	 * Remueve un arco e y retorna su r�tulo.
+	 * Remueve un arco e y retorna su rotulo.
 	 * @param e Un arco
- 	 * @return r�tulo de E.
-	 * @throws InvalidEdgeException si el arco es inv�lido.
+ 	 * @return rotulo de E.
+	 * @throws InvalidEdgeException si el arco es invalido.
 	 */
 	public E removeEdge(Edge<E> e) throws InvalidEdgeException;
 }
